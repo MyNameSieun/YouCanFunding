fdde2140886965164d7a8c1f832dc3834036a242
박시은 <charm516@naver.com>
2024-02-07 02:45:35 +0900

    frist commit

---
+++ src/redux/config/configStore.js
@@ -0,0 +1,8 @@
+// src/configStore.js
+import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({});
+const store = createStore(rootReducer);
+
+export default store;
