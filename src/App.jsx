677ed703a2fb066dddb7152c51ce17385014a040
박시은 <charm516@naver.com>
2024-02-08 11:42:47 +0900

    Style: 글로벌 스타일 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@
 import React, { useEffect } from 'react';
 import Router from 'shared/Router';
 import { app } from './firebase';
+import GlobalStyle from 'styles/GlobalStyle';
 
 function App() {
   useEffect(() => {
     console.log('app', app);
   }, []);
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
