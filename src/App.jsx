7154ca03a4e3c74b085b3bc7560fc3eb1b9747cb
HY965 <hyeonj965@gmail.com>
2024-02-08 14:27:35 +0900

    파이어베이스 초기설정수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 // App.js
-import React from 'react';
+import React, { useEffect } from 'react';
+import { app } from './firebase';
 import Router from 'shared/Router';
 import GlobalStyle from 'styles/GlobalStyle';
 
 function App() {
+  useEffect(() => {
+    console.log('app', app);
+  }, []);
   return (
     <>
       <GlobalStyle />
