48656f706c09e247b3064fa0412f0a4edd08438d
GNooNJo <jjjchoon@nate.com>
2024-02-08 12:02:39 +0900

    Feat: 각 페이지 라우팅 버튼 추가

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <>
+      <Link to="/main">
+        <button>메인 페이지</button>
+      </Link>
+      <Link to="/login">
+        <button>로그인 페이지</button>
+      </Link>
+      <Link to="/signup">
+        <button>회원가입 페이지</button>
+      </Link>
+      <Link to="/mypage">
+        <button>마이 페이지</button>
+      </Link>
+      <Link to="/register">
+        <button>등록 페이지</button>
+      </Link>
+      <Link to="/post/:id">
+        <button>세부 페이지</button>
+      </Link>
+    </>
+  );
+}
+
+export default Home;
