0af57d7c4c9614a100d4bb9dc2c92201cb615d89
GNooNJo <jjjchoon@nate.com>
2024-02-08 11:59:33 +0900

    Chore: firebase 테스트 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 // App.js
-import React, { useEffect } from 'react';
+import React from 'react';
 import Router from 'shared/Router';
-import { app } from './firebase';
 import GlobalStyle from 'styles/GlobalStyle';
 
 function App() {
-  useEffect(() => {
-    console.log('app', app);
-  }, []);
   return (
     <>
       <GlobalStyle />
