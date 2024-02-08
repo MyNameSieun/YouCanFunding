0c32bd671313a634b31e18bc6160a1a20c868984
박시은 <charm516@naver.com>
2024-02-08 15:52:07 +0900

    Style: 색상 전역 스타일링 적용

--- src/componentes/common/Navbar.jsx
+++ src/componentes/common/Navbar.jsx
@@ -3,34 +3,53 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   height: 90px;
+  width: 100%;
+  display: flex;
+`;
+const ColorBlue = styled.span`
+  color: var(--main-color);
+`;
+const NavBar = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LeftNav = styled.ul`
   display: flex;
 
-  align-items: center;
-
-  & ul {
-    display: flex;
-  }
   & li {
     margin-right: 30px;
     font-size: 28px;
     font-weight: bold;
   }
 `;
-const ColorBlue = styled.div`
-  color: #3425de;
-  display: inline;
+
+const RightNav = styled.ul`
+  display: flex;
+
+  & li {
+    margin-right: 30px;
+    font-size: 16px;
+  }
 `;
 
 function Navbar() {
   return (
     <Container>
-      <ul>
-        <li>
-          유캔<ColorBlue>FUN</ColorBlue>딩
-        </li>
-        <li>펀딩 예정</li>
-        <li>펀딩 종료</li>
-      </ul>
+      <NavBar>
+        <LeftNav>
+          <li>
+            유캔<ColorBlue>FUN</ColorBlue>딩
+          </li>
+          <li>펀딩 예정</li>
+          <li>펀딩 종료</li>
+        </LeftNav>
+        <RightNav>
+          <li>로그인</li>
+          <li>회원가입</li>
+          <li>프로젝트 등록</li>
+        </RightNav>
+      </NavBar>
     </Container>
   );
 }
