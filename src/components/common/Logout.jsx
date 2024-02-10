86529e9ac59ec7a5a41b1fcb21fb3d0f164d8eb6
JIYOUNG <0803jiyoung99@gmail.com>
2024-02-10 23:07:30 +0900

    Feat: Logout - 로그아웃 기능 구현

--- src/components/common/Logout.jsx
+++ src/components/common/Logout.jsx
@@ -1,7 +1,22 @@
+import { auth } from '../../firebase';
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Logout() {
-  return <div>Logout</div>;
+  const navigate = useNavigate();
+
+  const logOut = async (event) => {
+    event.preventDefault();
+    try {
+      await signOut(auth);
+      navigate('/main');
+    } catch (error) {
+      console.log('Error with logout', error);
+    }
+  };
+
+  return <button onClick={logOut}>로그아웃</button>;
 }
 
 export default Logout;
