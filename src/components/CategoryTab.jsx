7b13b06928a9d6ff8f81a9a9a1053ce43bc20aa4
박시은 <charm516@naver.com>
2024-02-10 14:27:33 +0900

    Feat: 펀딩 상태 필터링 기능 구현

--- src/components/CategoryTab.jsx
+++ src/components/CategoryTab.jsx
@@ -26,24 +26,24 @@ function CategoryTab({ activeTab, setActiveTab }) {
   };
 
   const data = [
-    { category: '전체', content: '내용' },
-    { category: '기술·가전', content: '내용' },
-    { category: '푸드', content: '내용' },
-    { category: '패션', content: '내용' },
-    { category: '뷰티', content: '내용' },
-    { category: '클래스', content: '내용' },
-    { category: '반려동물', content: '내용' },
-    { category: '게임', content: '내용' },
-    { category: '웹툰·만화', content: '내용' },
-    { category: '음악', content: '내용' },
-    { category: '사진', content: '내용' },
-    { category: '영화', content: '내용' },
-    { category: '홈·리빙', content: '내용' },
-    { category: '예술', content: '내용' },
-    { category: '캐릭터·굿즈', content: '내용' },
-    { category: '디자인·문구', content: '내용' },
-    { category: '도서·전자책', content: '내용' },
-    { category: '키즈', content: '내용' }
+    { category: '전체' },
+    { category: '기술·가전' },
+    { category: '푸드' },
+    { category: '패션' },
+    { category: '뷰티' },
+    { category: '클래스' },
+    { category: '반려동물' },
+    { category: '게임' },
+    { category: '웹툰·만화' },
+    { category: '음악' },
+    { category: '사진' },
+    { category: '영화' },
+    { category: '홈·리빙' },
+    { category: '예술' },
+    { category: '캐릭터·굿즈' },
+    { category: '디자인·문구' },
+    { category: '도서·전자책' },
+    { category: '키즈' }
   ];
 
   return (
--- src/components/HomeVerticalCard.jsx
+++ src/components/HomeVerticalCard.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import ProductsList from 'data/products.json';
 
