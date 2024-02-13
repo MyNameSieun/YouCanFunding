48db397d73e2fb0d9053c557a87872fd61bdb169
JIYOUNG <0803jiyoung99@gmail.com>
2024-02-13 20:15:15 +0900

    Feat: 펀딩 종료 알림창 구현

--- src/components/post/CompletedNotification.jsx
+++ src/components/post/CompletedNotification.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function CompletedNotification() {
-  return <div>CompletedNotification</div>;
+  return (
+    <CompletedContainer>
+      <p>펀딩이 종료되었습니다.</p>
+    </CompletedContainer>
+  );
 }
 
 export default CompletedNotification;
+
+const CompletedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 30px auto;
+  line-height: 1.5;
+`;
