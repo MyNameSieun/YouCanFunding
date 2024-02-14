0da8169a5a3db1d602b2d00bc4dda7f022cfed02
JIYOUNG <0803jiyoung99@gmail.com>
2024-02-15 03:44:49 +0900

    Feat: 스크랩한 프로젝트 마이페이지 목록으로 추가

--- src/components/post/HeartButton.jsx
+++ src/components/post/HeartButton.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import HeartImg from '../../assets/heart.png';
 import EmptyHeartImg from '../../assets/empty-heart.png';
-import { useNavigate } from 'react-router-dom';
-import { addDoc, collection, getDocs, query, deleteDoc } from 'firebase/firestore';
+import { useNavigate, useParams } from 'react-router-dom';
+import { addDoc, collection, getDocs, query, deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { auth, db } from '../../firebase';
 
 const Heart = styled.img`
@@ -18,6 +18,7 @@ const StyledHeartButton = styled.div`
 function HeartButton() {
   const navigate = useNavigate();
   const [like, setLike] = useState(false);
+  const id = useParams().id;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,6 +38,16 @@ function HeartButton() {
     if (!currentUser) {
       alert('로그인이 필요합니다.');
       return navigate('/login');
+    }
+
+    const projectDocRef = doc(db, 'projects', id);
+
+    if (like) {
+      // 좋아요를 취소하면 isScrapped: false로 업데이트
+      await updateDoc(projectDocRef, { isScrapped: false });
+    } else {
+      // 좋아요를 누르면 isScrapped: true로 업데이트
+      await updateDoc(projectDocRef, { isScrapped: true });
     }
 
     const collectionRef = collection(db, 'like');
